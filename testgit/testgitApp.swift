e2ec132f503ad3816f427a8e8ed1a20713bb1952
Serena Pia Capasso <sery@10-20-55-71.0200.fed.it.iosda.org>
2024-10-11 12:22:26 +0200

    first commit

---
+++ testgit/testgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgitApp.swift
+//  testgit
+//
+//  Created by Serena Pia Capasso on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct testgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
